c407c4bede46eeaa18f52c610c747c48b6679d95
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-07-06 12:34:09 +0530

    Port changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;
+const port = 10000;
 
 app.use(bodyParser.json());
 
