894a01ad9b554b33b918b9ca85b5427e81889556
Arnab Bhowmik <arnabbhowmik019@gmail.com>
2024-07-06 12:40:12 +0530

    cors issue fixed

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = 10000;
 
 app.use(bodyParser.json());
+app.use(cors({ origin: '*' }));
 
 // Create a transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
